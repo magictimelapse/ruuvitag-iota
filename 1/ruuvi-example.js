4be1ec84745ef08bd24763dea6efa6224d155fa5
Michael Rissi <michael.rissi@gmail.com>
2018-12-25 11:17:42 +0100

    add example javascript to read ruuvitag. From: https://www.npmjs.com/package/node-ruuvitag

---
+++ 1/ruuvi-example.js
@@ -0,0 +1,9 @@
+const ruuvi = require('node-ruuvitag');
+
+ruuvi.on('found', tag => {
+  console.log('Found RuuviTag, id: ' + tag.id);
+  tag.on('updated', data => {
+    console.log('Got data from RuuviTag ' + tag.id + ':\n' +
+      JSON.stringify(data, null, '\t'));
+  });
+});
