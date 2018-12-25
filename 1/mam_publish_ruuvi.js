f50174728a64ef9338af763f2e548269ed7f731a
Michael Rissi <michael.rissi@gmail.com>
2018-12-25 12:09:57 +0100

    Add mam_publish_ruuvi.js.

---
+++ 1/mam_publish_ruuvi.js
@@ -0,0 +1,94 @@
+/*
+Author: Robert Lie (mobilefish.com)
+Adjusted by Michael Rissi to run on tangle, not testnet
+Reads from the ruuvitag, and sends the data to the tangle.
+
+
+This file will work on a computer or Raspberry Pi.
+The published data can be viewed using the mam_receive.js file or
+https://www.mobilefish.com/services/cryptocurrency/mam.html (Select option: Data receiver)
+Usage:
+1)  You can change the default settings: MODE, SIDEKEY, SECURITYLEVEL or TIMEINTERVAL
+    If you do, make the same changes in mam_receive.js file.
+2)  Start the app: node mam_publish.js
+More information:
+https://www.mobilefish.com/developer/iota/iota_quickguide_raspi_mam.html
+*/
+
+// IOTA stuff
+const Mam = require('../lib/mam.client.js');
+const IOTA = require('iota.lib.js');
+const moment = require('moment');
+//const iota = new IOTA({ provider: 'https://nodes.testnet.iota.org:443' });
+const iota = new IOTA({ provider: 'https://potato.iotasalad.org:14265' });
+
+const MODE = 'restricted'; // public, private or restricted
+const SIDEKEY = 'mysecret'; // Enter only ASCII characters. Used only in restricted mode
+const SECURITYLEVEL = 3; // 1, 2 or 3
+const TIMEINTERVAL  = 30; // seconds
+// Initialise MAM State
+let mamState = Mam.init(iota, undefined, SECURITYLEVEL);
+
+/// ruuvitag stuff
+const ruuvi = require('node-ruuvitag');
+// global variable ruuvi_data. This can be solved in a more js way...
+let ruuvi_data = {};
+ruuvi.on('found', tag => {
+    console.log('Found RuuviTag, id: ' + tag.id);
+    tag.on('updated', data => {
+	ruuvi_data = data;
+	//console.log('Got data from RuuviTag ' + tag.id + ':\n' +
+	//	    JSON.stringify(data, null, '\t'));
+    });
+});
+
+
+
+// Set channel mode
+if (MODE == 'restricted') {
+    const key = iota.utils.toTrytes(SIDEKEY);
+    mamState = Mam.changeMode(mamState, MODE, key);
+} else {
+    mamState = Mam.changeMode(mamState, MODE);
+}
+
+// Publish data to the tangle
+const publish = async function(packet) {
+    // Create MAM Payload
+    const trytes = iota.utils.toTrytes(JSON.stringify(packet));
+    const message = Mam.create(mamState, trytes);
+
+    // Save new mamState
+    mamState = message.state;
+    console.log('Root: ', message.root);
+    console.log('Address: ', message.address);
+
+    // Attach the payload.
+    await Mam.attach(message.payload, message.address);
+    return message.root;
+}
+
+const generateJSON = function() {
+    // Generate some random numbers simulating sensor data
+    const data = Math.floor((Math.random()*89)+10);
+    const dateTime = moment().utc().format('DD/MM/YYYY hh:mm:ss');
+    const json = {"data": ruuvi_data, "dateTime": dateTime};
+    return json;
+}
+
+const executeDataPublishing = async function() {
+    const json = generateJSON();
+    console.log("json=",json);
+
+    const root = await publish(json);
+    console.log("published data");
+    console.log(json.dateTime);
+    console.log(json.data);
+    console.log(root);
+    //console.log(`dateTime: ${json.dateTime}, data: ${json.data}, root: ${root}`);
+}
+
+// Start it immediately
+executeDataPublishing();
+
+setInterval(executeDataPublishing, TIMEINTERVAL*1000);
